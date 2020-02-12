e24a2f70a1926702c415126190dc00a473b07732
Anne Damsgaard <andamsgaard@gmail.com>
2020-02-12 14:31:49 +0100

    Refactored Design

---
+++ Refactored_Design/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactored_Design
+{
+    public class Program
+    {
+        
+        
+            public static void Main(string[] args)
+            {
+                IHeater heater = new Heater();
+                ITempSensor tempSensor = new TempSensor();
+
+
+                var ecs = new ECS(28, tempSensor, heater);
+
+                ecs.Regulate();
+
+                ecs.SetThreshold(20);
+
+                ecs.Regulate();
+            }
+        
+    }
+    
+}
---
+++ Refactored_Design/TempSensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactored_Design
+{
+    internal class TempSensor : ITempSensor
+
+    {
+    private Random gen = new Random();
+
+    public int GetTemp()
+    {
+        return gen.Next(-5, 45);
+    }
+
+    public bool RunSelfTest()
+    {
+        return true;
+    }
+    }
+}
